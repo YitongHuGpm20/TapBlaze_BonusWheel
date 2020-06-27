b638b0b1e75bfd004ff54e094c65a10c6324a158
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2020-06-27 07:21:08 -0700

    Built basic spin function

--- TapBlaze Bonus Wheel/Assets/Scripts/SpinWheel.cs
+++ TapBlaze Bonus Wheel/Assets/Scripts/SpinWheel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class SpinWheel : MonoBehaviour
@@ -26,13 +27,13 @@ public class SpinWheel : MonoBehaviour
     int finalAngle;
 
     [SerializeField]
-    TextMeshPro winText;
+    TextMeshProUGUI winText;
 
     //string[] prizeTypes = new string[] { "Life", "Brush", "Gem", "Hammer", "Coin" };
 
     void Start()
     {
-        //Defalt case
+        //Default case
         sectors[0] = new Sector("Life", 30, 20);
         sectors[1] = new Sector("Brush", 3, 10);
         sectors[2] = new Sector("Gem", 35, 10);
@@ -41,11 +42,46 @@ public class SpinWheel : MonoBehaviour
         sectors[5] = new Sector("Brush", 1, 20);
         sectors[6] = new Sector("Gem", 75, 5);
         sectors[7] = new Sector("Hammer", 1, 20);
+
+        coroutineAllowed = true;
     }
 
     void Update()
     {
-        
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && coroutineAllowed)
+            //StartCoroutine(Spin());
+    }
+
+    IEnumerator Spin()
+    {
+        coroutineAllowed = false;
+        randomValue = Random.Range(20, 30);
+        timeInterval = 0.1f;
+
+        for(int i = 0; i < randomValue; i++) //Slow down the wheel
+        {
+            transform.Rotate(0, 0, 22.5f);
+            if (i > Mathf.RoundToInt(randomValue * .5f))
+                timeInterval = .2f;
+            if (i > Mathf.RoundToInt(randomValue * .85f))
+                timeInterval = .4f;
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        if (Mathf.RoundToInt(transform.eulerAngles.z) % 45 != 0)
+            transform.Rotate(0, 0, 22.5f);
+
+        finalAngle = Mathf.RoundToInt(transform.eulerAngles.z);
+        int sectorNum = finalAngle / 45 + 1;
+        winText.text = "You win " + sectorNum.ToString();
+
+        coroutineAllowed = true;
+    }
+
+    public void PressButton()
+    {
+        if(coroutineAllowed)
+            StartCoroutine(Spin());
     }
 
     bool areDropChancesGood()
