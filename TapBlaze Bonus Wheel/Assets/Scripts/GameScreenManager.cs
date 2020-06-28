41a3d5396caa89f4bc45771c4441a9cdeed83865
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2020-06-28 01:07:49 -0700

    Added Restart and Toggle Options functions to GameScreen

---
+++ TapBlaze Bonus Wheel/Assets/Scripts/GameScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScreenManager : MonoBehaviour
+{
+    public GameObject options;
+    public GameObject optionsOnButton;
+    public GameObject optionsOffButton;
+
+    public void ToggleOptions()
+    {
+        options.SetActive(!options.activeSelf);
+        optionsOffButton.SetActive(!optionsOffButton.activeSelf);
+        optionsOnButton.SetActive(!optionsOnButton.activeSelf);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ TapBlaze Bonus Wheel/Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public GameObject settings;
+    public GameObject info;
+    public GameObject socialMedia;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToggleSettings()
+    {
+        settings.SetActive(!settings.activeSelf);
+    }
+    public void ToggleInfo()
+    {
+        info.SetActive(!info.activeSelf);
+    }
+    public void ShareButton()
+    {
+        socialMedia.SetActive(!socialMedia.activeSelf);
+    }
+
+    public void ToggleBGM()
+    {
+        GameManager.bgmOn = !GameManager.bgmOn;
+    }
+
+    public void ToggleSFX()
+    {
+        GameManager.sfxOn = !GameManager.sfxOn;
+    }
+}
