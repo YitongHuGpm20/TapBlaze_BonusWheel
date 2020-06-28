49734b53f0beeacab61beb85fa7662fe1cede723
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2020-06-27 17:34:34 -0700

    Created Item Sum Function
    The number of each item will be updated by the result of spinning

--- TapBlaze Bonus Wheel/Assets/Scripts/SpinWheel.cs
+++ TapBlaze Bonus Wheel/Assets/Scripts/SpinWheel.cs
@@ -20,20 +20,34 @@ public class SpinWheel : MonoBehaviour
         }
     }
 
+    public class Item
+    {
+        public string Type;
+        public int Amount;
+        
+        public Item(string type, int amount)
+        {
+            Type = type;
+            Amount = amount;
+        }
+    }
+
     Sector[] sectors = new Sector[8];
+    Item[] items = new Item[5];
+
     int randomValue;
     float timeInterval;
     bool coroutineAllowed;
     int finalAngle;
 
-    [SerializeField]
-    TextMeshProUGUI winText;
+    public TextMeshProUGUI winText;
+    public TextMeshProUGUI[] itemAmount;
 
     //string[] prizeTypes = new string[] { "Life", "Brush", "Gem", "Hammer", "Coin" };
 
-    void Start()
+    void Awake()
     {
-        //Default case
+        //Default wheel sections
         sectors[0] = new Sector("Life", 30, 20);
         sectors[1] = new Sector("Brush", 3, 10);
         sectors[2] = new Sector("Gem", 35, 10);
@@ -43,13 +57,22 @@ public class SpinWheel : MonoBehaviour
         sectors[6] = new Sector("Gem", 75, 5);
         sectors[7] = new Sector("Hammer", 1, 20);
 
+        //Default item status
+        items[0] = new Item("Life", 0);
+        items[1] = new Item("Brush", 0);
+        items[2] = new Item("Gem", 0);
+        items[3] = new Item("Hammer", 0);
+        items[4] = new Item("Coin", 0);
+
+        for (int i = 0; i < 5; i++)
+            itemAmount[i].text = items[i].Amount.ToString();
+
         coroutineAllowed = true;
     }
 
     void Update()
     {
-        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && coroutineAllowed)
-            //StartCoroutine(Spin());
+
     }
 
     IEnumerator Spin()
@@ -58,7 +81,8 @@ public class SpinWheel : MonoBehaviour
         randomValue = Random.Range(20, 30);
         timeInterval = 0.1f;
 
-        for(int i = 0; i < randomValue; i++) //Slow down the wheel
+        //Slow down the wheel
+        for (int i = 0; i < randomValue; i++) 
         {
             transform.Rotate(0, 0, 22.5f);
             if (i > Mathf.RoundToInt(randomValue * .5f))
@@ -68,12 +92,22 @@ public class SpinWheel : MonoBehaviour
             yield return new WaitForSeconds(timeInterval);
         }
 
+        //When wheel stops
         if (Mathf.RoundToInt(transform.eulerAngles.z) % 45 != 0)
             transform.Rotate(0, 0, 22.5f);
 
+        //Update result
         finalAngle = Mathf.RoundToInt(transform.eulerAngles.z);
         int sectorNum = finalAngle / 45 + 1;
         winText.text = "You win Prize #" + sectorNum.ToString();
+        for(int i = 0; i < 5; i++)
+        {
+            if(items[i].Type == sectors[sectorNum - 1].Type)
+            {
+                items[i].Amount += sectors[sectorNum - 1].Amount;
+                itemAmount[i].text = items[i].Amount.ToString();
+            }
+        }
 
         coroutineAllowed = true;
     }
